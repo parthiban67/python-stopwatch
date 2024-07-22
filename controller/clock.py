bdbb033a16860bc265c0e143c3035067dd512333
Parthiban <parthibansolomon67@gmail.com>
2024-07-22 21:43:24 +0530

    clock added, worker and waker developed

---
+++ controller/clock.py
@@ -0,0 +1,100 @@
+from threading import Thread,local,Condition,Lock,Event
+from time import sleep  
+
+class Clock():
+    
+    def __init__(self):
+        self.initiated = False
+        self.condition = Condition(Lock())
+        self.pauseDict = dict(pause=False)
+    
+    def start(self):
+        if not self.initiated:
+            self.initiated = True
+            self.worker = Worker(self.condition,self.pauseDict)
+            self.worker.start()
+        else:
+            waker = Waker(self.condition,self.pauseDict)
+            waker.start()
+    
+    def stop(self):
+        self.worker.doPause()
+    
+    def reset(self):
+        pass
+
+class TimeData(local):
+    
+    def __init__(self,hr,min,sec):
+        local.__init__(self)
+        self.hr = hr
+        self.min = min
+        self.sec = sec
+    
+    def getHr(self):
+        return self.hr
+    
+    def setHr(self,hr):
+        self.hr = hr
+        
+    def setMin(self, min):
+        self.min = min
+        
+    def getMin(self):
+        return self.min
+    
+    def setSec(self, sec):
+        self.sec = sec
+    
+    def getSec(self):
+        return self.sec
+    
+class Worker(Thread):
+    
+    def __init__(self,condition,pauseDict):
+        Thread.__init__(self)
+        self.condition = condition
+        self.daemon = True
+        self.data = TimeData(0,0,0)
+        self.pauseDict = pauseDict
+    
+    def doPause(self):
+        self.pauseDict['pause'] = True
+        
+    def run(self):
+        while True:
+            with self.condition:
+                while self.pauseDict['pause']:
+                    self.condition.wait()
+                sleep(1)
+                secReset = False
+                if self.data.getSec() < 59:
+                    self.data.setSec(self.data.getSec() + 1)
+                else :
+                    self.data.setSec(0)
+                    secReset = True
+                minReset = False    
+                if secReset:
+                    if self.data.getMin() < 59:
+                        self.data.setMin(self.data.getMin() + 1)
+                    else :
+                        self.data.setMin(0)
+                        minReset = True
+                if minReset:
+                    self.data.setHr(self.data.getHr() + 1)
+                    
+                print(str(self.data.getHr())+":"+str(self.data.getMin())+":"+str(self.data.getSec()))
+
+class Waker(Thread):
+    
+    def __init__(self,condition,pauseDict):
+        Thread.__init__(self)
+        self.daemon = True
+        self.condition = condition
+        self.pauseDict = pauseDict
+        
+    def run(self):
+        with self.condition:
+            if self.pauseDict['pause']:
+                self.pauseDict['pause'] = False
+                self.condition.notify()
--- controller/controller.py
+++ controller/controller.py
@@ -1,22 +1,24 @@
 from tkinter import ttk
+from .clock import Clock
 
 class Controller():
     
     def __init__(self,parent,width,height):
         self.width = width
         self.frame = ttk.Frame(parent,width=self.width,height=height)
+        self.clock = Clock()
         self.setupButtons()
         
     def setupButtons(self):
         buttonFrame = ttk.Frame(self.frame,width=self.width)
-        startBtn = ttk.Button(buttonFrame,text="Start")
+        startBtn = ttk.Button(buttonFrame,text="Start",command=lambda : self.clock.start())
         startBtn.grid(row=0,column=0,padx=5)
         
-        stopBtn = ttk.Button(buttonFrame,text="Stop")
+        stopBtn = ttk.Button(buttonFrame,text="Stop",command=lambda : self.clock.stop())
         stopBtn.grid(row=0,column=1,padx=5)
         
-        restartBtn = ttk.Button(buttonFrame,text="Restart")
-        restartBtn.grid(row=0,column=2,padx=5)
+        resetBtn = ttk.Button(buttonFrame,text="Reset")
+        resetBtn.grid(row=0,column=2,padx=5)
         
         buttonFrame.pack(fill='x',expand=True, pady=30, padx=10)
     
